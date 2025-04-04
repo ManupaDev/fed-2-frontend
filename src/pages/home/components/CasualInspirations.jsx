b684221e2d07e2330edf8c1fd4e867895a35bf12
ManupaDev <manupadevsuite@gmail.com>
2025-04-04 14:09:42 +0530

    Add new image assets and update image sources in CasualInspirations and HeroGrid components to utilize local files, enhancing performance and reducing external dependencies.

--- src/pages/home/components/CasualInspirations.jsx
+++ src/pages/home/components/CasualInspirations.jsx
@@ -13,14 +13,14 @@ function CasualInspirations() {
       </div>
       <div className="relative col-span-1 md:col-span-3 h-60 sm:h-80 rounded-2xl">
         <img
-          src="https://s3-alpha-sig.figma.com/img/5e14/3183/ca0df25c3d226a223269e70541e09760?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=KGwwSVVr37GRllzWBFQToWUrc2nhQ1803P8tympO92Rnk5MFV~HU~BbcG191tI3uqByBaXstAcri1SeKurP8r-1wqcn3GQk35yg4ICtcI6pytK2Sz0hYaxRgsn8R20lMWgvY0ClvyvCPApxhs6loZvJeAlfuOIO~iCM57FTZpmUZaQWY4Lza0-bEbexFJJtL3UN5jQ-UpVNzh4UKpqWH1yYKGDEyHOifPgZuPHAqBJSBPcDrW5kF6MW2ac77htW1EZqpgmRMP1~PvDXlxJJ5I87IiEjEwKeOi9-yPQTH4AdPky7zGtvVkcVvTKh5AQBj7h85Kc8IteWQ1SQU5RloGg__"
+          src="/assets/images/ca0df25c3d226a223269e70541e09760.png"
           alt="Casual inspirations outfit"
           className="rounded-2xl absolute top-0 left-0 w-full h-full object-cover"
         />
       </div>
       <div className="relative col-span-1 md:col-span-3 h-60 sm:h-80 rounded-2xl">
         <img
-          src="https://s3-alpha-sig.figma.com/img/b0b7/82c0/2a24c60e5479cec788203caf906828d8?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=U~oXRSwTEZ5tlTYy1QKBY9BDCK72muPgpRH-svtQy2yMgz2BAJ~zuPxmd5nhw2~zto-K2sQYqVacHsJazp6OkeEkCfckQ1OMaZla9NqdqB~KnDzZg1vmPbdU8utsnAkBFMBevvtr47SmrBTcHuD9TxS8hXDsOY8X1VBq4W8fcTe1e17z9P6XkMZ5VPVdAhAWE6dFN3s4RcjwQTWVFZtXhhCojJDLOzF7wbAmACdFvWZb-fixi7Z1v5BDloyr-SU1CwJllbXl16mmFFAN5nXuxv3QXX5K5TrBHhbETvjdmIpxB3tKKHvPYgYyz~mgljAJFuXssntZQ1F9XYDXQ0vA-A__"
+          src="/assets/images/2a24c60e5479cec788203caf906828d8.png"
           alt="Casual inspirations outfit"
           className="rounded-2xl w-full absolute top-0 left-0 object-top h-full object-cover"
         />
--- src/pages/home/components/HeroGrid.jsx
+++ src/pages/home/components/HeroGrid.jsx
@@ -4,7 +4,7 @@ function HeroGrid() {
       <div className="relative col-span-1 lg:col-span-2 rounded-2xl">
         <img
           src={
-            "https://s3-alpha-sig.figma.com/img/3071/b1fc/729091cd0452fb9d0b89106ceec16368?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=MCMcrumw-QFHyd-1gGlF8f61GhSNPlakY6I9gU-1A2RYHScKdyuxCjpAm525H98zjRNV1Hxee1W2oeQJPHIxn7QSeZklFhBU8v61BRcIUo9SyT7aZgMfI1fycM9HTq4fJarsdQg7PeuwKjQRzS2W3bMNP~wz-wbQv6A7lGnGzvke3uPCR24eYJ8zJ1k53lliZvKx0qDSyTMhVOOXI-UTW7ZwVO5ELB-Ez2tAjo3NPyIdXYcQwfwvbfg4WR1upABAq90GmZA2Vt24gwoZvn15JeuM5zeeFfLod1gtatpDqvDC97E7xxDI911yAT2~sL3neiVz9~S6jwm-tdyLnw9dqA__"
+            "/assets/images/729091cd0452fb9d0b89106ceec16368.png"
           }
           className="rounded-2xl w-full h-full object-cover"
           alt="hero"
@@ -23,7 +23,7 @@ function HeroGrid() {
       <div className="col-span-1 grid grid-rows-1 md:grid-rows-2 gap-4">
         <div className="rounded-2xl relative h-40 md:h-auto">
           <img
-            src="https://s3-alpha-sig.figma.com/img/17aa/3a2f/29a85f64d93c41afa6b64d31b3a88038?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=liyGnBWon2Ek4nQzQZfub~8iHGtAjIJ26ioT-Ctf84o150xxHyIMjqUrbFIIs9ocBB~Qp2tw~gGAvujBQ0FnjGU083aqlkv~tu3JBgM1a38hVTZYngFsrvZDfe9lBm-fo7D2M~Lw-LIQhIbPPHP9bo~oI1o3~b04G4BWt3Kmrl29-3gFl~xaqdc~POnJcqpmsKxHvhtAf~mROoYGPf4GJ0ZyM50pmwCd7nG-MwuoJFfbhTuh59cNAhmQmuz-JNJmG-nCuMqN0CuSsIzTCOejMkcR18EUaDupTjKIauS2~7qU-PEAs8w85G9TubROGq7osAAJayvpD8Oj1TICBdEItQ__"
+            src="/assets/images/29a85f64d93c41afa6b64d31b3a88038.png"
             alt="Featured product"
             className="rounded-2xl w-full h-full object-cover"
           />
@@ -35,7 +35,7 @@ function HeroGrid() {
         </div>
         <div className="rounded-2xl relative h-40 md:h-auto">
           <img
-            src="https://s3-alpha-sig.figma.com/img/837e/11f0/0233936f837e7b69d6a545511b1ba132?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=SRmUmMCKLNk1QYES2N7j~p3NvgFc5nYQwBj5rPQeFaBBFnMXf3b5Mjd~2RZZ1j69gtvkrZXLzEmOhOwGESBzFP1d2dCP0bvvuHylVS-3xedr~oQ86ca3B6qZbZ9bwBqARhDtJpxAm0HJQNEd-AFE4~T8SMttFxy74m36pPKxGTkJQBvqO1iPhYKRhNdkRFWUG2L84x1Ob7uyFi~dA6u5~nFq0~uigd9b0kCYk07nKlTV3iLtQzlnUNp9TPBeRuT4EYgOoUdtcmdCMONEnXhQGGtXBwI7cyPxiEgmcQcA63KBl422oTb5eMoxKTDXSrcI8KtW4uAhswgjKjnRxws1~w__"
+            src="/assets/images/0233936f837e7b69d6a545511b1ba132.png"
             alt="Featured product"
             className="rounded-2xl w-full h-full object-cover"
           />
